0c875afe18f282f8041056d28dcb1f566a43d2c8
garyCC227 <z5163479@student.unsw.edu.au>
2020-09-28 23:59:45 +1000

    try mapbox interactive map with react

---
+++ app/src/App.js
@@ -0,0 +1,60 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import mapboxgl from "mapbox-gl";
+
+mapboxgl.accessToken =
+  "pk.eyJ1IjoiZ2FyeWNjMjI3IiwiYSI6ImNrZDJyaWRxbDFnZGcycXF5dm8wcnptdjQifQ.Aw920Zjcv8rr6ewAj3oe1A";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      lng: 77.6887,
+      lat: 13.0194,
+      zoom: 17,
+    };
+  }
+
+  componentDidMount() {
+    const map = new mapboxgl.Map({
+      container: this.mapContainer,
+      style: "mapbox://styles/mapbox/satellite-v9",
+      center: [this.state.lng, this.state.lat],
+      zoom: this.state.zoom,
+    });
+
+    map.on("move", () => {
+      this.setState({
+        lng: map.getCenter().lng.toFixed(4),
+        lat: map.getCenter().lat.toFixed(4),
+        zoom: map.getZoom().toFixed(2),
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="sidebarStyle">
+          <div>
+            Longitude: {this.state.lng} | Latitude: {this.state.lat} | Zoom:{" "}
+            {this.state.zoom}
+          </div>
+        </div>
+        <div
+          ref={(el) => (this.mapContainer = el)}
+          className="mapContainer"
+          style={{
+            position: "absolute",
+            top: "0",
+            right: "0",
+            left: "0",
+            bottom: "0",
+          }}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
